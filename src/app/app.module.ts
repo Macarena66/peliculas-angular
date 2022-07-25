349a65d03f444e41688a0364fabd2a522ff0965b
CNA06P009\Tarde <macarenachaves@gmail.com>
2022-07-25 20:27:18 +0200

    primer commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PeliculasComponent } from './components/peliculas/peliculas.component';
+import { JuegoComponent } from './components/juego/juego.component';
+import { PeliculaAleatoriaComponent } from './components/pelicula-aleatoria/pelicula-aleatoria.component';
+import { AnfitrionesComponent } from './components/anfitriones/anfitriones.component';
+import { HeaderComponent } from './components/header/header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PeliculasComponent,
+    JuegoComponent,
+    PeliculaAleatoriaComponent,
+    AnfitrionesComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
